adab93d37ab750b3cd4bc89647a74a61a959d591
alex <abalitsky@maprtech.com>
2016-04-28 22:38:19 +0300

    lab3

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -3,16 +3,20 @@ global.api = api;
 api.net = require('net');
 
 var socket = new api.net.Socket();
-var user;
 
 socket.connect({
   port: 2000,
   host: '127.0.0.1',
 }, function() {
-  socket.write('Hello from client');
+  socket.write(JSON.stringify({ data: 'Hello from client' }));
   socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+    packet = JSON.parse(data);
+    console.log('Data received (by client): ' + packet.data);
+    if (Array.isArray(packet.data)) {
+      packet.data = packet.data.map(function (item) { return item *= 2; });
+      console.log('Data to send: ' + packet.data);
+      socket.write(JSON.stringify(packet));
+    }
+
   });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -2,12 +2,46 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11, 6];
+var result = [];
+var clients = [];
+var buffer = [];
+var count = 0;
+var numOfClients = 3;
 
 var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
   console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
+  socket.on('data', function (data) {
+    packet = JSON.parse(data);
+    console.log('Data received (by server): ' + packet.data);
+    if (Array.isArray(packet.data)) {
+      buffer[packet.tId] = packet.data;
+      count++;
+      if (count == numOfClients) {
+        count = 0;
+        for (var i = 0; i < buffer.length; i++) {
+          console.log(buffer.length);
+          result.push.apply(result, buffer[i]);
+        }
+        console.log('Result data: ' + result);
+      }
+    }
+
   });
 }).listen(2000);
+
+server.on('connection', function (socket) {
+
+  clients.push(socket);
+  console.log('Number of clients: ' + clients.length);
+
+  if (clients.length == numOfClients) {
+    var sizeForClient = task.length / numOfClients;
+    for (var i = 0; i < clients.length; i++) {
+      var packet = {};
+      packet.data = task.slice(i * sizeForClient, (i + 1) * sizeForClient);
+      packet.tId = i;
+      clients[i].write(JSON.stringify(packet));
+    }
+  }
+});
